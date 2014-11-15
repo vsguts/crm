8396a70367124e2e83b28629ad80a594520e5e1d
vsguts <vsguts@gmail.com>
2014-11-15 13:57:32 +0300

    core.js added.

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,6 +21,7 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'js/core.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
---
+++ web/js/core.js
@@ -0,0 +1,15 @@
+(function($){
+    
+    $(function(){
+        console.log('loaded');
+    });
+
+    $(document).on('click', function(e) {
+        var jelm = $(e.target);
+
+        if (jelm.is('.m-toggle')) {
+            
+        }
+    });
+
+})(jQuery);

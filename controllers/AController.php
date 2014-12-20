5f8d2c67bb887786add686fe0e8e8905e00b4501
vsguts <vsguts@gmail.com>
2014-12-20 16:41:44 +0300

    Browser language detection impemented.

--- controllers/AController.php
+++ controllers/AController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use Yii;
 use yii\web\Controller;
+use app\models\Language;
 
 class AController extends Controller
 {
@@ -16,6 +17,19 @@ class AController extends Controller
 
         if ($language = $session->get('language')) {
             Yii::$app->language = $language;
+        } else {
+            if (!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+                $languages = Language::find()->orderBy(['name' => SORT_ASC])->all();
+                $codes = [];
+                foreach ($languages as $language) {
+                    $codes[] = $language->code;
+                }
+                if (preg_match("/(" . implode('|' , $codes) . ")+(-|;|,)?(.*)?/", $_SERVER['HTTP_ACCEPT_LANGUAGE'], $matches)) {
+                    $browser_language = $matches[1];
+                    Yii::$app->session->set('language', $browser_language);
+                    Yii::$app->language = $browser_language;
+                }
+            }
         }
     }
 }

55cdc08d451d1d0a39925d94141b3d273d2cab47
vsguts <vsguts@gmail.com>
2016-09-27 09:17:00 +0300

    deploy.sh: Mysqldump was added to deploy process.

---
+++ commands/MysqlController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\commands;
+
+use Yii;
+use yii\console\Controller;
+use yii\db\Query;
+
+/**
+ * Mysql console wrapper
+ */
+class MysqlController extends Controller
+{
+    /**
+     * Mysqldump wrapper
+     *
+     * @param  string $path Path
+     */
+    public function actionMysqldump($path)
+    {
+        $db_comp = Yii::$app->db;
+
+        $db_name = (new Query)->select('database()')->scalar();
+
+        exec(sprintf('mysqldump -u%s -p%s %s > %s', $db_comp->username, $db_comp->password, $db_name, $path));
+    }
+
+}
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+mkdir -p files/dumps
+./app mysql/mysqldump files/dumps/`date +'%Y%m%d_%H%M%S'`.sql
+
 git pull --rebase
 
-composer self-update
-composer update
+# composer self-update
+# rm -rf ./vendor
+composer install
 
-./app migrate/up --interactive=0
+./yii migrate/up --interactive=0
+./app tools/rbac
+
+# Cache
+#rm web/css/app.css*
+#rm web/css/site.css*

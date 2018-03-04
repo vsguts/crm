1453cfecbe3107055efbaac64f9508333916e376
vsguts <vsguts@gmail.com>
2018-03-04 23:14:42 +0200

    index.php script was removed from repo.

--- web/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
-$config = require(__DIR__ . '/../bootstrap/web.php');
-
-(new yii\web\Application($config))->run();
